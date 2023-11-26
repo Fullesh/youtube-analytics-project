8b023429e126626cdeb4b3fcd7c10c7a59624249
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-26 19:51:02 +0300

    As part of the fourth homework, a Video class and initialized with real data was created

---
+++ src/video.py
@@ -0,0 +1,16 @@
+from googleapiclient.discovery import build
+import os
+import json
+
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+class Video:
+    def __init__(self, video_id):
+        self.video_id = video_id
+        self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.video_id
+                                               ).execute()
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = 'www.youtube.com/watch?='+self.video_id
+        self.views = self.video_response['items'][0]['statistics']['viewCount']
+        self.likes = self.video_response['items'][0]['statistics']['likeCount']

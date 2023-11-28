ddcc54e7e80e697e1138c2e65fa26b01d9379aa6
Danila Belov <danilabelov.adm@yandex.ru>
2023-11-28 20:02:44 +0300

    Updated video.py. Reason: PEP8 fixes

--- src/video.py
+++ src/video.py
@@ -2,16 +2,17 @@ from googleapiclient.discovery import build
 import os
 import json
 
+
 class Video:
     def __init__(self, video_id):
         self.api_key: str = os.getenv('YT_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.video_id = video_id
         self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
+                                                         id=self.video_id
+                                                         ).execute()
         self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = 'www.youtube.com/watch?='+self.video_id
+        self.video_url = 'www.youtube.com/watch?=' + self.video_id
         self.views = self.video_response['items'][0]['statistics']['viewCount']
         self.likes = self.video_response['items'][0]['statistics']['likeCount']
 

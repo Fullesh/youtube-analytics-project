cbbc7497f5a494a083c7db5f209502086a2c55ab
Danila Belov <danilabelov.adm@yandex.ru>
2023-12-03 18:07:56 +0300

    Added exceptions for uncorrect video id

--- src/video.py
+++ src/video.py
@@ -8,16 +8,25 @@ class Video:
         self.api_key: str = os.getenv('YT_API_KEY')
         self.youtube = build('youtube', 'v3', developerKey=self.api_key)
         self.video_id = video_id
-        self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                         id=self.video_id
-                                                         ).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = 'www.youtube.com/watch?=' + self.video_id
-        self.views = self.video_response['items'][0]['statistics']['viewCount']
-        self.likes = self.video_response['items'][0]['statistics']['likeCount']
+        try:
+            self.video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                             id=self.video_id
+                                                             ).execute()
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = 'www.youtube.com/watch?=' + self.video_id
+            self.views = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print('Несуществующий id видео')
+        finally:
+            self.video_response = None
+            self.title = None
+            self.video_url = None
+            self.views = None
+            self.like_count = None
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
@@ -27,4 +36,4 @@ class PLVideo(Video):
         print(json.dumps(self.video_response, indent=4))
 
     def __str__(self):
-        return self.video_title
+        return self.title
